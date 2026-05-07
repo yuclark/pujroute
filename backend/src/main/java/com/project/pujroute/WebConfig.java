eed85c642621cf1d16dd8593eb7eee86c6068941
Vince Clark B. Lanticse <lanticsev@gmail.com>
2026-05-07 20:33:07 +0800

    Restored imports

--- backend/src/main/java/com/project/pujroute/WebConfig.java
+++ backend/src/main/java/com/project/pujroute/WebConfig.java
@@ -1,3 +1,9 @@
+package com.project.pujroute;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
